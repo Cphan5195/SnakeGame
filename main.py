38978cf027b52ff4fe0f4d943a5a336e9ab160f3
backpackbrain <backpackbrain@gmail.com>
2022-05-17 17:20:35 +1000

    Added a scoreboard that displays the top 3 highest scores reached when the player dies, using file on disk functionality

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import thorpy  # S: imported thorpy
 import time
 from pygame.locals import KEYDOWN, K_RIGHT, K_LEFT, K_UP, K_DOWN, K_ESCAPE
 from pygame.locals import QUIT
-
+import pickle #S: added pickle module for high scores
 from game import Game
 
 black = pygame.Color(0, 0, 0)
@@ -71,20 +71,9 @@ def message_display(text, x, y, color=black):
     screen.blit(text_surf, text_rect)
     # pygame.display.update() #solved for blink bug on the front page
 
-# S: Duplicated message_display in order to use a separate function to display the crash score when the player loses
-
-
-def message_display_crash(text, x, y, color=black):
-    # S: Smaller font than the original
-    large_text = pygame.font.SysFont('comicsansms', 30)
-    text_surf, text_rect = text_objects(text, large_text, color)
-    text_rect.center = (x, y)
-    screen.blit(text_surf, text_rect)
-    pygame.display.update()
-
 #S: Duplicated message_display in order to use a separate function to display the crash score when the player loses
 def message_display_crash(text, x, y, color=black):
-    large_text = pygame.font.SysFont('comicsansms', 30)#S: Smaller font than the original
+    large_text = pygame.font.SysFont('comicsansms', 20)#S: Smaller font than the original
     text_surf, text_rect = text_objects(text, large_text, color)
     text_rect.center = (x, y)
     screen.blit(text_surf, text_rect)
@@ -117,18 +106,30 @@ def quitgame():
 def crash():
     pygame.mixer.Sound.play(crash_sound)
     screen.blit(background, (0, 0))
-
-    # S: added crashscore to print to player the score they got
-    crashscore = str(game.snake_score())
-    # S: added crashstring to print to player with good formatting
-    crashstring = 'Crashed: Your score was ' + crashscore
-    # S: replaced original string with crashstring, calling the duplicated function
-    message_display_crash(crashstring, game.settings.width /
-                          2 * 15, game.settings.height / 3 * 15, white)
-    time.sleep(5)  # S: made the text stay longer on screen
-    crashscore = str(game.snake_score())#S: added crashscore to print to player the score they got
-    crashstring = 'Crashed: Your score was ' + crashscore #S: added crashstring to print to player with good formatting
+    crashscore = str(game.snake_score()) #S: added crashscore to print to player the score they got
+    try:
+        with open('highscores.dat', 'rb') as file:#S: opens any existing high scores
+            highscores = pickle.load(file)
+    except:
+        highscores = [0] * 3#S: minimum of three scores for the top three to prevent errors in access
+    '''
+    S: Using the pickle method of storing the high scores as an array, the above code loads the existing high scores unless there are none, in which case the array is turned into zeroes
+    '''
+    highscores.append(game.snake_score())#S: appends the current score to the list of high scores
+    highscores = sorted(highscores, reverse=True)#S: sorts the high scores from highest to lowest
+       
+    crashstring = 'Crashed: Your score was ' + crashscore #S: added crashstring to print to player with good formatting 
     message_display_crash(crashstring, game.settings.width / 2 * 15, game.settings.height / 3 * 15, white) #S: replaced original string with crashstring, calling the duplicated function
+    message_display_crash("TOP THREE BEST SCORES", game.settings.width / 2 * 15, game.settings.height / 3 * 18, white)#S: heading of scoreboard
+    firststring = '#1: ' + str(highscores[0]))#S: accessing sorted array to find first place score
+    secondstring = '#2: ' + str(highscores[1])#S: accessing sorted array to find second place score
+    thirdstring = '#3: ' + str(highscores[2]))#S: accessing sorted array to find third place score
+    message_display_crash(firststring, game.settings.width / 2 * 15, game.settings.height / 3 * 21, white)#S: displaying first place score
+    message_display_crash(secondstring, game.settings.width / 2 * 15, game.settings.height / 3 * 24, white)#S: displaying second place score
+    message_display_crash(thirdstring, game.settings.width / 2 * 15, game.settings.height / 3 * 27, white)#S: displaying third place score
+    with open('highscores.dat', 'wb') as file:#S: stores the high score array in the file so that it is stored on disk and not just when the program is executed
+        pickle.dump(highscores, file)
+    
     time.sleep(5)#S: made the text stay longer on screen
 
 
@@ -149,7 +150,7 @@ def initial_interface():
                         2 * 15, game.settings.height / 4 * 15)
 
         menu.react(event)#S: thorpy ui code, which will be used later with variable set by user
-            
+        
 
         screen.blit(background, (0, 0))#S: this is the background variable as earlier used, to improve how the screen is displayed
         message_display('Gluttonous', game.settings.width / 2 * 15, game.settings.height / 4 * 15)
