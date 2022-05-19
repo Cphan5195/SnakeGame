3c00eb32e3382000a9a558fd2e917c5bb2fa4b0c
backpackbrain <backpackbrain@gmail.com>
2022-05-19 21:25:14 +1000

    Commented on the python functions and their meanings

--- game.py
+++ game.py
@@ -4,20 +4,31 @@ Created on Wed Apr 25 15:19:25 2018
 
 @author: zou
 """
+'''
+S: This is the imported modules, with pygame involving most of the game functions including image loading, screen display, movement.
+The random module is used when placing down the strawberries and choosing which graphical representation is displayed, for user freshness.
+Numpy module runs mathematical calculations which are used to determine positions of snakes and items.
+'''
 import pygame
 import random
-import thorpy  # S: imported thorpy
+import thorpy  # S: imported thorpy, a graphical module for testing
 import numpy as np
 
-
+#S: This class simply defines the initial size and thus relative size of most of the objects in both scripts.
 class Settings:
     def __init__(self):
         self.width = 28
         self.height = 28
         self.rect_len = 15
 
-
+#S: This class defines everything to do with the snake the player controls.
 class Snake:
+    '''
+S: The below function initialises the snake with images for what the head looks like in all directions 
+as well as the tail in all directions, and the body which only has one direction. It starts the snake facing right
+and then calls the initialise function.
+    '''
+
     def __init__(self):
 
         self.image_up = pygame.image.load('images/head_up.bmp')
@@ -34,16 +45,21 @@ class Snake:
 
         self.facing = "right"
         self.initialize()
+        
+    '''
+    S: This function starts the snake at a fixed coordinate and starts it off with three segments to the left of it
+(   including the head as one) and a score of zero
+    '''
 
     def initialize(self):
         self.position = [6, 6]
         self.segments = [[6 - i, 6] for i in range(3)]
         self.score = 0
 
-    def blit_body(self, x, y, screen):
+    def blit_body(self, x, y, screen):#S: A simple function that visually displays the body of the snake at a given coordinate
         screen.blit(self.image_body, (x, y))
 
-    def blit_head(self, x, y, screen):
+    def blit_head(self, x, y, screen):#S: A simple function that displays the snake head in the same direction it is facing
         if self.facing == "up":
             screen.blit(self.image_up, (x, y))
         elif self.facing == "down":
@@ -53,9 +69,9 @@ class Snake:
         else:
             screen.blit(self.image_right, (x, y))
 
-    def blit_tail(self, x, y, screen):
+    def blit_tail(self, x, y, screen):#S: A simple function that checks the tail's direction and appropriately displays the tail image
         tail_direction = [self.segments[-2][i] -
-                          self.segments[-1][i] for i in range(2)]
+                          self.segments[-1][i] for i in range(2)]#S: a mathematical algorithm that will return specific results for the direction of the tail
 
         if tail_direction == [0, -1]:
             screen.blit(self.tail_up, (x, y))
@@ -66,15 +82,15 @@ class Snake:
         else:
             screen.blit(self.tail_right, (x, y))
 
-    def blit(self, rect_len, screen):
-        self.blit_head(self.segments[0][0]*rect_len,
+    def blit(self, rect_len, screen):#S: A function that displays every portion of the snake appropriately
+        self.blit_head(self.segments[0][0]*rect_len,#S: displays head
                        self.segments[0][1]*rect_len, screen)
-        for position in self.segments[1:-1]:
+        for position in self.segments[1:-1]:#S:ensuring the body does not overlap onto the head or tail displaying
             self.blit_body(position[0]*rect_len, position[1]*rect_len, screen)
-        self.blit_tail(self.segments[-1][0]*rect_len,
+        self.blit_tail(self.segments[-1][0]*rect_len,#S: displays tail
                        self.segments[-1][1]*rect_len, screen)
 
-    def update(self):
+    def update(self):#S: A function that moves the snake in the direction it is facing
         if self.facing == 'right':
             self.position[0] += 1
         if self.facing == 'left':
@@ -83,10 +99,11 @@ class Snake:
             self.position[1] -= 1
         if self.facing == 'down':
             self.position[1] += 1
-        self.segments.insert(0, list(self.position))
+        self.segments.insert(0, list(self.position))#S: moves the position of the segments accordingly with the snake's movement
 
-
+#S: Below is a class containing the data of the fruit (strawberry) that gives the player points.
 class Strawberry():
+#S: Below is the initialisation of the fruit, which includes the settings for size, style (which decides the image), and starts the first fruit
     def __init__(self, settings):
         self.settings = settings
 
@@ -95,7 +112,8 @@ class Strawberry():
             'images/food' + str(self.style) + '.bmp')
         self.initialize()
 
-    def random_pos(self, snake):
+    def random_pos(self, snake):#S: This function gives a random fruit image to the fruit and finds a random coordinate within the boundary 
+    #that is not within the snake already, and places the fruit there.
         self.style = str(random.randint(1, 8))
         self.image = pygame.image.load(
             'images/food' + str(self.style) + '.bmp')  # edit food photo
@@ -111,16 +129,17 @@ class Strawberry():
 
     def blit(self, screen):
         screen.blit(
-            self.image, [p * self.settings.rect_len for p in self.position])
+            self.image, [p * self.settings.rect_len for p in self.position])#S: A simple function that displays the fruit image
 
-    def initialize(self):
+    def initialize(self):#S: starts the first fruit at the same place
         self.position = [15, 10]
 
-
+#S: A major function that defines the elements of the game and is commonly called in the main function.
 class Game:
     """
+    S: The game initialises with the settings as initially described, a snake as seen by the snake class, a strawberry with the same settings
+    as initially described, and a dictionary variable that transforms the key input into relevant input for determining the directions and moves of the snake.
     """
-
     def __init__(self):
         self.settings = Settings()
         self.snake = Snake()
@@ -130,11 +149,12 @@ class Game:
                           2: 'left',
                           3: 'right'}
 
-    def restart_game(self):
+    def restart_game(self):#S: A function to restart the snake and the strawberry completely.
         self.snake.initialize()
         self.strawberry.initialize()
-
-    def current_state(self):
+    
+    def current_state(self):#S: A function which returns an array that corresponds to the positions of the snake and the strawberries.
+    #S: Removing this function has no effect on the game, but there was also no opportunity to implement something with this function, so it has no actual utility.
         state = np.zeros((self.settings.width+2, self.settings.height+2, 2))
         expand = [[0, 1], [0, -1], [-1, 0], [1, 0],
                   [0, 2], [0, -2], [-2, 0], [2, 0]]
@@ -151,10 +171,17 @@ class Game:
                   self.strawberry.position[0]+d[1], 1] = 0.5
         return state
 
-    def direction_to_int(self, direction):
+    def direction_to_int(self, direction):#S: A function called in main's human_move() that converts player input into readable input for the dict containing directions
         direction_dict = {value: key for key, value in self.move_dict.items()}
         return direction_dict[direction]
 
+    '''
+    S: The below fuction takes the current direction and gets the input direction, and if the direction is possible 
+    i.e. if it is a 90 degree turn and not an 180 degree turn, the new direction is the current direction and the snake is updated.
+    This function also checks if there is a fruit in the snake's body (inc. head) and adds to the score if so, returning a value 
+    that reflects whether there was a fruit eaten or not.
+    The function also returns differently if the game has ended via the game end function.    
+    '''
     def do_move(self, move):
         move_dict = self.move_dict
 
@@ -185,7 +212,7 @@ class Game:
 
         return reward
 
-    def game_end(self):
+    def game_end(self):#S: A simple 'death' function that returns true if the snake has crashed on any wall or any portion of its own body 
         end = False
         if self.snake.position[0] >= self.settings.width or self.snake.position[0] < 0:
             end = True
@@ -196,10 +223,10 @@ class Game:
 
         return end
 
-    def blit_score(self, color, screen):
+    def blit_score(self, color, screen):#S: A simple function to display the current score of the user.
         font = pygame.font.SysFont(None, 25)
         text = font.render('Score: ' + str(self.snake.score), True, color)
         screen.blit(text, (0, 0))
 
-    def snake_score(self):  # S: Added a function that should return the current snake score
+    def snake_score(self):  # S: Added a function that should return the current snake score as a safe way of doing so.
         return self.snake.score
--- main.py
+++ main.py
@@ -5,6 +5,17 @@ Created on Wed May 16 15:22:20 2018
 @author: zou
 """
 
+"""
+S: Below is the imports that are needed for the main function of the game
+cgitb is a module used to enable traceback tracking in Python scripts
+tkinter is a standard python graphical user interface module
+turtle involves basic sprite movement in python
+thorpy is a graphical module added for user interactions
+pygame is a game based module, which involves input from users as seen in the keys imported
+pickle is a module used to store variables to disk i.e. high scores
+game is the other python script which contains the Game class among other relevant functions
+"""
+
 from cgitb import grey
 from tkinter import N
 from turtle import back
@@ -16,11 +27,14 @@ from pygame.locals import QUIT
 import pickle  # S: added pickle module for high scores
 from game import Game
 
+
+'''
+S: Below are the colours used in the game, set as pygame.Color variables using RGB formatting
+'''
 black = pygame.Color(0, 0, 0)
 # add game background
 gamebackground = pygame.Color(124, 194, 72)
 white = pygame.Color(255, 255, 255)
-
 green = pygame.Color(0, 200, 0)
 bright_green = pygame.Color(0, 255, 0)
 red = pygame.Color(200, 0, 0)
@@ -31,6 +45,13 @@ yellow = pygame.Color(255, 205, 0)
 bright_yellow = pygame.Color(255, 255, 0)
 gray = pygame.Color(80, 80, 80)
 
+'''
+S: Below is the initialisation of the game via calling the Game class, and setting the length 
+of the snake rectangle as well as initialising the snake controlled by the Snake class
+and initialising them with the pygame.init() call. the fpsClock is linked to the passage of time and thus 
+the speed of the game is linked how fast the game runs per second of real time.
+The background is filled and the intro screen which displays Gluttonous, the game title, is set.
+'''
 game = Game()
 rect_len = game.settings.rect_len
 snake = game.snake
@@ -50,22 +71,27 @@ pygame.display.set_caption('Gluttonous')
 
 thorpy.theme.set_theme('human')
 line = thorpy.Line(400, 'h')
-
-
 menu = thorpy.Menu()
-# Sharang: trying out thorpy for GUI element addition. Above is a test slider and below is initialising it, go to thorpy documentation to understand better
+# [Sharang: trying out thorpy for GUI element addition. Above is a test slider and below is initialising it, go to thorpy documentation to understand better]
+#S: These are now legacy elements that simply add to the UI but do not have functionality with regards to difficulty which the above comment would imply
 for element in menu.get_population():
     element.surface = screen
 
 
-crash_sound = pygame.mixer.Sound('./sound/crash.wav')
+crash_sound = pygame.mixer.Sound('./sound/crash.wav')#S: This is the noise that plays when a player crashes their snake.
 
-
+'''
+S: The below function renders a given text string onto a text surface in pygame and returns the relevant text surface
+along with the rectangle it should be contained in.
+'''
 def text_objects(text, font, color=black):
     text_surface = font.render(text, True, color)
     return text_surface, text_surface.get_rect()
 
-
+'''
+S: The below function takes text and coordinates and uses the text_objects function to display that text at the given 
+position and at a given font size.
+'''
 def message_display(text, x, y, color=black):
     large_text = pygame.font.SysFont('comicsansms', 50)
     text_surf, text_rect = text_objects(text, large_text, color)
@@ -74,7 +100,8 @@ def message_display(text, x, y, color=black):
 
     # pygame.display.update()  # solved for blink bug on the front page
 
-# S: Duplicated message_display in order to use a separate function to display the crash score when the player loses
+# S: Duplicated message_display in order to use a separate function to display the crash score when the player loses with slightly different variables
+#The functionality is identical to the original
 
 
 def message_display_crash(text, x, y, color=black):
@@ -85,7 +112,12 @@ def message_display_crash(text, x, y, color=black):
     screen.blit(text_surf, text_rect)
     pygame.display.update()
 
-
+'''
+S: The below function uses pygame functionality such as mouse.get_pos() to set up a button in pygame which displays
+a given message at a given coordinate with a given width and height, with different colours when the buttons are active
+or inactive. It changes the colour when the mouse is over the button, and if it is clicked, then it executes any parameter
+that is given (such as the game loop for the start button or the quitgame function for the quit button)
+'''
 def button(msg, x, y, w, h, inactive_color, active_color, action=None, parameter=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
@@ -104,7 +136,7 @@ def button(msg, x, y, w, h, inactive_color, active_color, action=None, parameter
     TextRect.center = (x + (w / 2), y + (h / 2))
     screen.blit(TextSurf, TextRect)
 
-
+#S: A simple function that quits pygame scripts and python scripts when called.
 def quitgame():
     pygame.quit()
     quit()
@@ -136,10 +168,11 @@ def level_page():
 # *****************************************************************
 
 
+#S: As this function was modified, it is thoroughly commented; it handles the events when a player's snake crashes.
 def crash():
-    pygame.mixer.Sound.play(crash_sound)
-    screen.blit(background, (0, 0))
-    # S: added crashscore to print to player the score they got
+    pygame.mixer.Sound.play(crash_sound)#S: plays the crash audio file
+    screen.blit(background, (0, 0))#S: displays a different background after crashing
+    # S: added crashscore to print to player the score they got in string form
     crashscore = str(game.snake_score())
     try:
         with open('highscores.dat', 'rb') as file:  # S: opens any existing high scores
@@ -148,7 +181,8 @@ def crash():
         # S: minimum of three scores for the top three to prevent errors in access
         highscores = [0] * 3
     '''
-    S: Using the pickle method of storing the high scores as an array, the above code loads the existing high scores unless there are none, in which case the array is turned into zeroes
+    S: Using the pickle method of storing the high scores as an array, the above code loads the existing
+    high scores unless there are none, in which case the array is turned into three zeroes 
     '''
     highscores.append(
         game.snake_score())  # S: appends the current score to the list of high scores
@@ -179,9 +213,14 @@ def crash():
         pickle.dump(highscores, file)
 
     time.sleep(5)  # S: made the text stay longer on screen
-    initial_interface()
+    initial_interface()#S: loads the initial interface after the time has passed
 
 
+'''
+S: The below function defines the initial interface for the game, or the intro, and checks if the quit button is called and quits if so.
+The go and quit buttons are initiated with the button function, and the title is displayed with the message_display function.
+The screen is then loaded using the pygame display and time functions, and it remains stable until a button is clicked.
+'''
 def initial_interface():
     intro = True
     while intro:
@@ -189,7 +228,8 @@ def initial_interface():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            # S: thorpy ui code, which will be used later with variable set by user
+            # S: thorpy ui code, [which will be used later with variable set by user]
+            #S: This is now purely cosmetic
             menu.react(event)
 
         # S: this is the background variable as earlier used, to improve how the screen is displayed
@@ -205,6 +245,11 @@ def initial_interface():
         pygame.time.Clock().tick(15)
 
 
+"""
+S: The game loop function, which has been modified by Tien, runs the functions related to movement of the snake and visual
+graphics of the snake, as well as placing the strawberry items on the screen via calling the relevant game functions from 
+the other python script, and calling crash() if the game has ended.
+"""
 def game_loop(player, fps=10):  # fps start with 10
     speed = 0
     try:
@@ -239,7 +284,10 @@ def game_loop(player, fps=10):  # fps start with 10
 
     crash()
 
-
+'''
+S: This function accepts the key down input from the player and transforms it into string variables to return to the
+game loop so that it can appropriately move the snake with the game script's functions. 
+'''
 def human_move():
     direction = snake.facing
 
@@ -262,6 +310,6 @@ def human_move():
     move = game.direction_to_int(direction)
     return move
 
-
+#S: Runs the python script from the initial interface function if it is called by the name of its own function.
 if __name__ == "__main__":
     initial_interface()
